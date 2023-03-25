324a3af08f6d0faf68ad04f92cdf3ff3efcb6710
Jair Deyner <jairdeyner730791@gmail.com>
2023-03-24 21:10:29 -0500

    Finish

--- src/main/java/com/bolsadeideas/springboot/web/app/SpringBootWebApplication.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/SpringBootWebApplication.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.springboot.app.springbootweb;
+package com.bolsadeideas.springboot.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
